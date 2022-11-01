0344faac658d763ac0a7ba2929a7d3713c66a70b
Saphanley Saint-Vil <108836179+SinisterSaint@users.noreply.github.com>
2022-11-01 14:56:09 -0400

    update

---
+++ Section_19/Section_19.1/flask-greet-calc-solution/greet/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    return "welcome"
+
+@app.route('/welcome/back')
+def welcome_back():
+    return "welcome back"
+
+@app.route('/welcome/home')
+def welcome_home():
+    return "welcome home"
