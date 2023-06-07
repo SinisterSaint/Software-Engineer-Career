61235de60313c77dcac37f62554e2856a5b10b84
Saphanley Saint-Vil <108836179+SinisterSaint@users.noreply.github.com>
2023-06-07 10:28:02 -0400

    update

--- section_50/Food-Recipe-App/food-recipe-app/API/db.js
+++ section_50/Food-Recipe-App/food-recipe-app/API/db.js
@@ -1,11 +1,19 @@
 const { Client } = require("pg");
 require("dotenv").config();
 
-const USER = process.env.USER;
-const PASSWORD = process.env.PASSWORD;
-const HOST = process.env.HOST;
-const DBPORT = process.env.DBPORT;
-const DATABASE = process.env.DATABASE;
+// const USER = process.env.USER;
+// const PASSWORD = process.env.PASSWORD;
+// const HOST = process.env.HOST;
+// const DBPORT = process.env.DBPORT;
+// const DATABASE = process.env.DATABASE;
+
+
+const USER="saphanley";
+const PASSWORD="prophetsaint1120";
+const HOST="localhost";
+const DBPORT="5432";
+const DATABASE="recipe_app";
+const API_PORT="3000";
 
 let db;
 
