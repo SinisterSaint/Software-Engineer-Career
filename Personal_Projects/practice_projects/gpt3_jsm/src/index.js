55b86bf8167873b80c5fbdc3ef717d396553c6de
Saphanley Saint-Vil <108836179+SinisterSaint@users.noreply.github.com>
2023-09-09 00:31:26 -0400

    update

--- Personal_Projects/practice_projects/gpt3_jsm/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-// The reactDOM will allow us to hook our react application to our index.html
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'
-
-ReactDOM.render(<App />, document.getElementById('root'));
